503ee7c12c8b8aeee5c8a3b79fe317f74606ea69
rbhagat32 <raghavbhagat32@gmail.com>
2025-03-21 18:25:58 +0530

    adding tailwind next

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -19,6 +19,7 @@ export default function Button({ children, onClick }: PropTypes) {
       onClick={onClick}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
+      className="bg-zinc-900 px-4 py-2 text-white"
     >
       {children}
     </button>
