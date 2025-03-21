0c4b2c6743fde95daad4a2a518f8cd232d0519a3
rbhagat32 <raghavbhagat32@gmail.com>
2025-03-21 19:10:49 +0530

    added customizability on button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,6 +4,7 @@ interface PropTypes {
   children: React.ReactNode;
   altText?: React.ReactNode;
   bgColor?: string;
+  className?: string;
   onClick?: () => void;
 }
 
@@ -11,6 +12,7 @@ export default function Button({
   children,
   altText = children,
   bgColor = "bg-white",
+  className,
   onClick,
 }: PropTypes) {
   return (
@@ -18,7 +20,7 @@ export default function Button({
       onClick={onClick}
       className={`group/btn relative cursor-pointer overflow-hidden h-[42px] w-fit flex flex-col items-center rounded-full ${bgColor} ${
         bgColor === "" && "border text-zinc-100"
-      } text-black font-bold shadow-lg shadow-zinc-400`}
+      } text-black font-bold shadow-lg shadow-zinc-400 ${className}`}
     >
       <div className="w-full h-[42px] group-hover/btn:-translate-y-[40px] px-5 py-2 transition-all duration-300 ease-in-out">
         {children}
