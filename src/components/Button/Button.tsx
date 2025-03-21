fe8a8d1cffefdae0255625ff32b97f96bc540e2f
rbhagat32 <raghavbhagat32@gmail.com>
2025-03-21 19:19:12 +0530

    added button variants

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,24 +3,33 @@ import React from "react";
 interface PropTypes {
   children: React.ReactNode;
   altText?: React.ReactNode;
-  bgColor?: string;
-  className?: string;
+  variant?: "fill" | "outline";
   onClick?: () => void;
 }
 
 export default function Button({
   children,
   altText = children,
-  bgColor = "bg-white",
-  className,
+  variant = "fill",
   onClick,
 }: PropTypes) {
-  return (
+  return variant === "fill" ? (
     <button
       onClick={onClick}
-      className={`group/btn relative cursor-pointer overflow-hidden h-[42px] w-fit flex flex-col items-center rounded-full ${bgColor} ${
-        bgColor === "" && "border text-zinc-100"
-      } text-black font-bold shadow-lg shadow-zinc-400 ${className}`}
+      className="group/btn relative cursor-pointer overflow-hidden h-[42px] w-fit flex flex-col items-center rounded-full bg-white text-black font-bold shadow-lg shadow-zinc-400"
+    >
+      <div className="w-full h-[42px] group-hover/btn:-translate-y-[40px] px-5 py-2 transition-all duration-300 ease-in-out">
+        {children}
+      </div>
+
+      <div className="w-full h-[42px] group-hover/btn:-translate-y-[40px] px-5 py-2 transition-all duration-300 ease-in-out">
+        {altText}
+      </div>
+    </button>
+  ) : (
+    <button
+      onClick={onClick}
+      className="group/btn relative cursor-pointer overflow-hidden h-[42px] w-fit flex flex-col items-center rounded-full bg-transparent border text-zinc-100 text-black font-bold shadow-lg shadow-zinc-400"
     >
       <div className="w-full h-[42px] group-hover/btn:-translate-y-[40px] px-5 py-2 transition-all duration-300 ease-in-out">
         {children}
